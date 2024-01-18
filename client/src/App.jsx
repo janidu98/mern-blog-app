238f055b3914e2c091ce4f81b10352ea09471120
janidu98 <janinduchathushan@gmail.com>
2024-01-18 11:32:25 +0530

    create header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Projects from "./components/pages/Projects"
 import Dashboard from "./components/pages/Dashboard"
 import Singin from "./components/pages/Singin"
 import Singup from "./components/pages/Singup"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
