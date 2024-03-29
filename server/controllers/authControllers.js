e604811f23ff9ee6eda1cfa630b989b6d49bb232
janidu98 <janinduchathushan@gmail.com>
2024-03-29 23:33:49 +0530

    added comment for understanding

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -4,8 +4,10 @@ import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
+  //request the username, email and password from the frontend
   const { username, email, password } = req.body;
-
+  
+  //check all fields are filled or not
   if (
     !username ||
     !email ||
@@ -16,7 +18,8 @@ export const signup = async (req, res, next) => {
   ) {
     next(errorHandler(400, "All fields are required!"));
   }
-
+  
+  //hash the entered password
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
@@ -35,23 +38,27 @@ export const signup = async (req, res, next) => {
 };
 
 export const signin = async (req, res, next) => {
+  //request the email and password from the frontend
   const { email, password } = req.body;
-
+  //check whether email and password are empty or not
   if (!email || !password || email === "" || password === "") {
     return next(errorHandler(400, "All fields are required!"));
   }
 
   try {
+    //check user is valid or not
     const validUser = await User.findOne({ email });
     if (!validUser) {
       return next(errorHandler(400, "User not found"));
     }
 
+    //check password is correct or not
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, "Invalid Password"));
     }
 
+    //create jwt webtoken
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
     //remove the password from result
